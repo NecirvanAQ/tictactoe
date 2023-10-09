0eb724a70d2dfa154d4e553c2357091a00fd8c24
Necirvan <AQ231029@aquinas.ac.uk>
2023-10-09 18:46:53 +0100

    some fixes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +13,19 @@ class Player
 
     public static void Play(List<int> places)
     {
-        Console.WriteLine("Player 1: ");
+        Console.WriteLine("Your move: ");
         int place = Convert.ToInt32(Console.ReadLine());
         places.Add(place);
+    }
+
+    public static bool Won(List<int> places)
+    {
+        if (places.Contains(00) && places.Contains(01) && places.Contains(02))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
 
@@ -23,15 +34,19 @@ class Program
     static void Main(string[] args)
     {
         Player player1 = new Player();
+        Player player2 = new Player();
+        
 
-        while (true)
+        while (Player.Won(player1.places) == false || Player.Won(player2.places) == false)
         {
-            DrawGrid(player1);
+            DrawGrid(player1, player2);
             Player.Play(player1.places);
+            DrawGrid(player1, player2);
+            Player.Play(player2.places);
         }
         
     }
-    static void DrawGrid(Player player1)
+    static void DrawGrid(Player player1, Player player2)
     {
         Console.Clear();
         Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -41,12 +56,17 @@ class Program
         grid.Add(new List<string> { "", "", "" });
         grid.Add(new List<string> { "", "", "" });
 
-        foreach(int place in player1.places)
+        foreach (int place in player1.places)
+        {
+            grid[(place / 10) % 10][place % 10] = "O";
+        }
+
+        foreach (int place in player2.places)
         {
             grid[(place / 10) % 10][place % 10] = "X";
         }
 
-        foreach(var list in grid)
+        foreach (var list in grid)
         {
             Console.WriteLine("__________");
 
